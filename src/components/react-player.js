d2e508a1cc3bc72fa9741ac56cf6e2f578072d19
jyk524 <67247717+jyk524@users.noreply.github.com>
2020-10-15 00:10:04 -0400

    added carousel with responsive video

---
+++ src/components/react-player.js
@@ -0,0 +1,19 @@
+import React from "react"
+import ReactPlayer from "react-player"
+
+function App() {
+  return (
+    <div>
+      <ReactPlayer
+      className='react-player'
+      width='100%'
+      height='100%'
+      url="https://youtu.be/LEItInhQtSY"
+      />
+    </div>
+  )
+}
+
+
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Carousel } from 'antd';
+import ReactPlayer from "react-player"
+
+
+function onChange(a, b, c) {
+  console.log(a, b, c);
+}
+
+const contentStyle = {
+  height: 'calc(480px + 14.2vw)',
+  color: '#fff',
+  lineHeight: '360px',
+  textAlign: 'center',
+  background: '#364d79',
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div>
+  <Carousel afterChange={onChange}>
+    <div>
+      <h3 style={contentStyle}>
+      <ReactPlayer
+      className='react-player'
+      width='100%'
+      height='100%'
+      url="https://youtu.be/LEItInhQtSY"
+      volume={.1}
+      />
+      </h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>Second Slide</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>Third Slide</h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}>Fourth Slide</h3>
+    </div>
+  </Carousel>
+  </div>,
+  document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
